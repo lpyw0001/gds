1e30448a1992eed4ccfb12f527b2faae442e9194
lpyw0001 <82916448+lpyw0001@users.noreply.github.com>
2021-10-24 13:28:12 +1100

    draft 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,29 +4,47 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
 
-	var	$window = $(window),
+	var $window = $(window),
 		$body = $('body');
 
 	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1141px',  '1680px' ],
-			large:    [ '981px',   '1140px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '321px',   '480px'  ],
-			xxsmall:  [ null,      '320px'  ]
-		});
+	breakpoints({
+		xlarge: ['1141px', '1680px'],
+		large: ['981px', '1140px'],
+		medium: ['737px', '980px'],
+		small: ['481px', '736px'],
+		xsmall: ['321px', '480px'],
+		xxsmall: [null, '320px']
+	});
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	$window.on('load', function () {
+		window.setTimeout(function () {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
 
 	// Scrolly.
-		$('.scrolly').scrolly();
+	$('.scrolly').scrolly();
+
+
+	// From https://codepen.io/matthewcain/pen/ZepbeR
+	var btn = $('#button');
+
+	$(window).scroll(function () {
+		if ($(window).scrollTop() > 300) {
+			btn.addClass('show');
+		} else {
+			btn.removeClass('show');
+		}
+	});
+
+	btn.on('click', function (e) {
+		e.preventDefault();
+		$('html, body').animate({ scrollTop: 0 }, '300');
+	});
+
 
 })(jQuery);
